8f7ce7e2b6daf107e0f1f8c6e7c6c989699e910e
Cindy <cindy@cindy-Lenovo-G480.(none)>
2016-04-01 13:39:52 +0800

    added fields

--- eval/dms/content/document.py
+++ eval/dms/content/document.py
@@ -20,6 +20,8 @@ from plone.app.textfield import RichText
 from z3c.relationfield.schema import RelationList, RelationChoice
 from plone.formwidget.contenttree import ObjPathSourceBinder
 from plone.multilingualbehavior.directives import languageindependent
+from plone.autoform.directives import write_permission, read_permission
+from plone.formwidget.multifile import MultiFileFieldWidget
 from collective import dexteritytextindexer
 
 from eval.dms import MessageFactory as _
@@ -31,6 +33,22 @@ class IDocument(form.Schema, IImageScaleTraversable):
     """
     Document
     """
+
+    dexteritytextindexer.searchable('title')
+    title = schema.TextLine(
+        title = _(u"Title"),
+        required = False,
+    )
+
+    dexteritytextindexer.searchable('multifile')
+    write_permission(multifile='cmf.ReviewPortalContent')
+    read_permission(multifile='cmf.ReviewPortalContent')
+    form.widget(multifile=MultiFileFieldWidget)
+    multifile = schema.List(
+            title=_(u"Document"),
+            required=False,
+            value_type=NamedBlobFile(),
+        )
     pass
 
 alsoProvides(IDocument, IFormFieldProvider)
