2ed83916c1e416cbdcf1f03606ac665e522bab64
Cindy <cindy@cindy-Lenovo-G480.(none)>
2016-05-16 17:51:55 +0800

    added view

--- eval/workspace/browser/folder_view.py
+++ eval/workspace/browser/folder_view.py
@@ -10,3 +10,30 @@ class Index(dexterity.DisplayForm):
     grok.template('folder_view')
     grok.name('view')
 
+    @property
+    def catalog(self):
+    	return getToolByName(self.context, 'portal_catalog')
+
+    def contents(self):
+    	context = self.context
+    	catalog = self.catalog
+    	path = '/'.join(context.getPhysicalPath())
+    	results = []
+    	brains = catalog.searchResults(path={'query': path, 'depth':1},
+    					portal_type='eval.workspace.document_file',
+    					sort_on='id', 
+    					sort_order='reverse' )
+    	return brains
+
+    def searchedValue(self, name=None):
+        result = 0
+        if self.request.form.has_key('data'):
+            form = self.request.form
+            result = form[name]
+        return result
+
+    def roles(self, obj=None):
+        current = str(api.user.get_current())
+        roles = api.user.get_roles(username=current, obj= obj)
+        allowed =  ['Owner','Manager', 'Administrator'] 
+        return any((True for x in roles if x in allowed))
